4a9d3f9ede12b61f070b0f5dd02f6b98ea0bbcbc
Benedikt Wehner <benedikt.wehner@outlook.de>
2024-07-13 20:01:19 +0200

    implement search food search bar

--- app/src/main/java/com/bennewehn/triggertrace/ui/food/FoodScreen.kt
+++ app/src/main/java/com/bennewehn/triggertrace/ui/food/FoodScreen.kt
@@ -1,26 +1,36 @@
 package com.bennewehn.triggertrace.ui.food
 
 import android.content.res.Configuration
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.bennewehn.triggertrace.R
 import com.bennewehn.triggertrace.ui.theme.TriggerTraceTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FoodScreen(
     modifier: Modifier = Modifier,
@@ -38,29 +48,57 @@ fun FoodScreen(
         }
     ) { innerPadding ->
         Column(
-            modifier = Modifier.padding(innerPadding)
-        ){
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .padding(horizontal = 25.dp),
+        ) {
+            val searchQuery by viewModel.searchQuery.collectAsState()
+            val isSearchBarActive by viewModel.isSearchBarActive.collectAsState()
+            SearchBar(
+                query = searchQuery,
+                onQueryChange = viewModel::updateSearchQuery,
+                onSearch = {},
+                active = isSearchBarActive,
+                onActiveChange = viewModel::updateIsSearchBarActive,
+                placeholder = { Text(text = stringResource(id = R.string.search_meal_or_ingredient)) },
+                windowInsets = WindowInsets(top = 0.dp),
+                leadingIcon = { Icon(Icons.Filled.Search, null) },
+                trailingIcon = {
+                    if (isSearchBarActive) {
+                        Icon(
+                            modifier = Modifier.clickable {
+                                viewModel.updateIsSearchBarActive(false)
+                                viewModel.updateSearchQuery("")
+                            },
+                            imageVector = Icons.Default.Close,
+                            contentDescription = null
+                        )
+                    }
+                }
+            ) {
+            }
         }
     }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-private fun FoodTopAppBar(onBack: () -> Unit){
+private fun FoodTopAppBar(onBack: () -> Unit) {
     TopAppBar(
         title = { Text(text = stringResource(id = R.string.food_screen_title)) },
         navigationIcon = {
             IconButton(onClick = onBack) {
                 Icon(Icons.AutoMirrored.Filled.ArrowBack, stringResource(id = R.string.back_button))
             }
-        },
+        }
     )
 }
 
 @Preview(name = "Food Screen Preview Dark", uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Preview(name = "Food Screen Preview Light")
 @Composable
-private fun FoodScreenPreview(){
+private fun FoodScreenPreview() {
     TriggerTraceTheme {
         FoodScreen(onBack = {}, onAddFood = {})
     }
--- app/src/main/java/com/bennewehn/triggertrace/ui/food/FoodViewModel.kt
+++ app/src/main/java/com/bennewehn/triggertrace/ui/food/FoodViewModel.kt
@@ -1,25 +1,27 @@
 package com.bennewehn.triggertrace.ui.food
 
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class FoodViewModel @Inject constructor(): ViewModel() {
 
-    //private val _name = MutableLiveData("")
-    //val name: LiveData<String> = _name
+    private val _searchQuery = MutableStateFlow("")
+    val searchQuery: StateFlow<String> = _searchQuery.asStateFlow()
 
-    var name1 by mutableStateOf("hello")
+    private val _isSearchBarActive= MutableStateFlow(false)
+    val isSearchBarActive: StateFlow<Boolean> = _isSearchBarActive.asStateFlow()
 
-    var name2 = MutableStateFlow("name2")
+    fun updateSearchQuery(query: String) {
+        _searchQuery.value = query
+    }
 
-    fun updateName2(newState: String) {
-        name2.value = newState
+    fun updateIsSearchBarActive(active: Boolean) {
+        _isSearchBarActive.value = active
     }
 
 }

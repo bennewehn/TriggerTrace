d89578883df45f817a4499c365973aa2450cd532
Benedikt Wehner <benedikt.wehner@outlook.de>
2024-08-12 21:56:48 +0200

    implement food selection view

--- app/src/main/java/com/bennewehn/triggertrace/ui/food/FoodScreen.kt
+++ app/src/main/java/com/bennewehn/triggertrace/ui/food/FoodScreen.kt
@@ -1,14 +1,24 @@
 package com.bennewehn.triggertrace.ui.food
 
 import android.content.res.Configuration
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Card
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -17,6 +27,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -24,7 +35,9 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.bennewehn.triggertrace.R
+import com.bennewehn.triggertrace.data.Food
 import com.bennewehn.triggertrace.ui.components.FoodSearchBar
 import com.bennewehn.triggertrace.ui.components.FoodSearchBarViewModel
 import com.bennewehn.triggertrace.ui.theme.TriggerTraceTheme
@@ -36,11 +49,17 @@ fun FoodScreen(
     onAddFood: () -> Unit,
     viewModel: FoodViewModel = hiltViewModel(),
 ) {
+
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
     FoodScreenContent(
         modifier = modifier,
         onBack = onBack,
         onAddFood = onAddFood,
-        foodSearchBarViewModel = viewModel.foodSearchBarViewModel
+        foodSearchBarViewModel = viewModel.foodSearchBarViewModel,
+        uiState = uiState,
+        onFoodSelected = viewModel::onFoodSelected,
+        onFoodItemSelectionChanged = viewModel::onFoodItemSelectionChanged
     )
 }
 
@@ -50,7 +69,10 @@ private fun FoodScreenContent(
     modifier: Modifier,
     onBack: () -> Unit,
     onAddFood: () -> Unit,
-    foodSearchBarViewModel: FoodSearchBarViewModel?
+    onFoodSelected: (Food) -> Unit,
+    onFoodItemSelectionChanged: (Boolean, SelectableFood) -> Unit,
+    foodSearchBarViewModel: FoodSearchBarViewModel?,
+    uiState: FoodScreenUIState
 ) {
     Scaffold(
         modifier = modifier,
@@ -72,9 +94,31 @@ private fun FoodScreenContent(
             FoodSearchBar(
                 leadingIcon = Icons.Default.Search,
                 placeHolder = stringResource(id = R.string.search_food),
-                onFoodSelected = {},
+                onFoodSelected = onFoodSelected,
                 viewModel = foodSearchBarViewModel
             )
+
+            Spacer(modifier = Modifier.height(50.dp))
+
+            Card {
+                LazyColumn {
+                    items(uiState.selectedFoods) { selectableFood ->
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .clickable { },
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Checkbox(
+                                checked = selectableFood.selected,
+                                onCheckedChange = { checked -> onFoodItemSelectionChanged(checked, selectableFood) }
+                            )
+                            Spacer(modifier = Modifier.width(5.dp))
+                            Text(text = selectableFood.food.name)
+                        }
+                    }
+                }
+            }
         }
     }
 }
@@ -101,7 +145,16 @@ private fun FoodScreenPreview() {
             modifier = Modifier,
             onBack = {},
             onAddFood = {},
-            foodSearchBarViewModel = null
+            foodSearchBarViewModel = null,
+            uiState = FoodScreenUIState(
+                selectedFoods = listOf(
+                    SelectableFood(Food(name = "Milk"), selected = true),
+                    SelectableFood(Food(name = "Chocolate"), selected = false),
+                    SelectableFood(Food(name = "Water"), selected = true),
+                )
+            ),
+            onFoodSelected = {},
+            onFoodItemSelectionChanged = {_, _ ->}
         )
     }
 }
--- app/src/main/java/com/bennewehn/triggertrace/ui/food/FoodViewModel.kt
+++ app/src/main/java/com/bennewehn/triggertrace/ui/food/FoodViewModel.kt
@@ -1,16 +1,81 @@
 package com.bennewehn.triggertrace.ui.food
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.bennewehn.triggertrace.data.Food
 import com.bennewehn.triggertrace.data.FoodRepository
 import com.bennewehn.triggertrace.ui.components.FoodSearchBarViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
+
+data class SelectableFood(
+    val food: Food,
+    val selected: Boolean
+)
+
+data class FoodScreenUIState(
+    val selectedFoods: List<SelectableFood> = emptyList(),
+)
 
 @HiltViewModel
 class FoodViewModel @Inject constructor(
     foodRepository: FoodRepository
-): ViewModel() {
+) : ViewModel() {
 
     val foodSearchBarViewModel = FoodSearchBarViewModel(foodRepository)
+
+    private val _uiState = MutableStateFlow(FoodScreenUIState())
+    val uiState: StateFlow<FoodScreenUIState> = _uiState.asStateFlow()
+
+    // Job reference for background work
+    private var debounceJob: MutableMap<SelectableFood, Job> = mutableMapOf()
+
+
+    fun onFoodSelected(food: Food) {
+        foodSearchBarViewModel.updateSearchBarActive(false)
+
+        val containsFood = _uiState.value.selectedFoods.any { it.food == food }
+        if (!containsFood) {
+            _uiState.update {
+                it.copy(
+                    selectedFoods = it.selectedFoods + SelectableFood(food, true)
+                )
+            }
+        }
+    }
+
+    fun onFoodItemSelectionChanged(selected: Boolean, selectableFood: SelectableFood) {
+        // Cancel the previous job if it exists
+        debounceJob[selectableFood]?.cancel()
+
+        debounceJob[selectableFood] = viewModelScope.launch {
+            // Update the UI state immediately
+            _uiState.value = _uiState.value.copy(
+                selectedFoods = _uiState.value.selectedFoods.map {
+                    if (it.food == selectableFood.food) {
+                        it.copy(selected = selected)
+                    } else {
+                        it
+                    }
+                }
+            )
+
+            delay(1000)
+
+            // Check the updated state and conditionally remove the item
+            _uiState.value = _uiState.value.copy(
+                selectedFoods = _uiState.value.selectedFoods.filter { item ->
+                    item.food != selectableFood.food || item.selected
+                }
+            )
+        }
+    }
 
 }
